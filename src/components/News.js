ca5cb9c027a4d6bfaab269c8cf0081c5e0e1d6d0
victorr0909 <namit09092001@gmail.com>
2023-05-06 20:16:03 +0530

    Fetched api

--- src/components/News.js
+++ src/components/News.js
@@ -41,6 +41,14 @@ export default class News extends Component {
     };
   }
 
+  async componentDidMount(){
+    let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=8aa6021f50ec495c9a289ca271d5631c";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({articles : parsedData.articles})
+
+  }
+
   render() {
     return (
       <div className="container my-3">
@@ -49,8 +57,8 @@ export default class News extends Component {
           {this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
               <NewsItem
-                title={element.title.slice(0,45)}
-                description={element.description.slice(0,88)}
+                title={element.title?element.title:""}
+                description={element.description?element.description:""}
                 imageUrl={element.urlToImage}
                 newsUrl={element.url}
               />
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,10 +8,10 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width : "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={!imageUrl?"https://image.cnbcfm.com/api/v1/image/107236197-1683275209822-gettyimages-1237080749-BC_2021IMXHK_6569.jpeg?v=1683278957&w=1920&h=1080":imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">{title}...</h5>
-                    <p className="card-text">{description}...</p>
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
                     <a href={newsUrl} target='_blank' className="btn btn-primary btn-sm">Read More</a>
                 </div>
         </div>
