a1fd90644eb1737f7b62345f4dade969ee439a64
victorr0909 <namit09092001@gmail.com>
2023-05-08 00:47:02 +0530

    Modified Header of the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-
 import './App.css';
-
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
--- src/components/News.js
+++ src/components/News.js
@@ -77,7 +77,7 @@ export default class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center">NewsMonkey - Top Headlines</h1>
+        <h1 className="text-center">NewsMonkey - Top Headlines For you!</h1>
         <div className="row">
           {this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
