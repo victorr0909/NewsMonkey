6e8b187bc6f89aaa4cc7313f345443945d794ed8
victorr0909 <namit09092001@gmail.com>
2023-05-10 23:57:30 +0530

    Added Cateogories and linked to respective APIs pt 2

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -16,9 +16,9 @@ export class NewsItem extends Component {
             alt="..."
           />
           <div className="card-body">
+          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:'1',left:'90%'}}>{source}</span>
             <h5 className="card-title">
               {title}
-              <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{source}</span>
             </h5>
             <p className="card-text">{description}</p>
             <p class="card-text">
