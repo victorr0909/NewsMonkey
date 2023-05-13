60aef064c71576b1eabd7a66a3a8686b1cdac275
victorr0909 <namit09092001@gmail.com>
2023-05-13 20:46:42 +0530

    refactored NewsItem Component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,40 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date , source} = this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <img
-            src={
-              !imageUrl
-                ? "https://image.cnbcfm.com/api/v1/image/107236197-1683275209822-gettyimages-1237080749-BC_2021IMXHK_6569.jpeg?v=1683278957&w=1920&h=1080"
-                : imageUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:'1',left:'90%'}}>{source}</span>
-            <h5 className="card-title">
-              {title}
-            </h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text">
-              <small className="text-body-secondary">
-                By {!author ? "Unknown" : author} at{" "}
-                {new Date(date).toGMTString()}
-              </small>
-            </p>
-            <a href={newsUrl} target="_blank" className="btn btn-dark btn-sm">
-              Read More
-            </a>
-          </div>
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <img
+          src={
+            !imageUrl
+              ? "https://image.cnbcfm.com/api/v1/image/107236197-1683275209822-gettyimages-1237080749-BC_2021IMXHK_6569.jpeg?v=1683278957&w=1920&h=1080"
+              : imageUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <span
+            className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+            style={{ zIndex: "1", left: "90%" }}
+          >
+            {source}
+          </span>
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              By {!author ? "Unknown" : author} at{" "}
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a href={newsUrl} target="_blank" className="btn btn-dark btn-sm">
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItem;
