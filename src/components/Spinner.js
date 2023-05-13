9ffcac22e38ab9793262dae434dd5bc7768fa5d1
victorr0909 <namit09092001@gmail.com>
2023-05-13 20:47:39 +0530

    Converted to Functional component

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react'
 import loading from './loading.gif'
 
-export default class Spinner extends Component {
-  render() {
+const Spinner = ()=>{
     return (
       <div className='text-center'>
         <img alt='loading' src={loading}></img>
       </div>
     )
   }
-}
+export default Spinner
